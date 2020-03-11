cb3b16a440dc0c2ca77807daa940e32e7399b2fc
YeahZou <2544610309@qq.com>
2020-03-11 12:15:45 -0700

    根据基金代码获取基金基本信息

--- YeahFund/sina.py
+++ YeahFund/sina.py
@@ -6,16 +6,18 @@ import logging
 import time
 import re
 import sys
-import json
 import demjson
 import math
 
-FUND_LIST_URL = 'http://vip.stock.finance.sina.com.cn/fund_center/data/jsonp.php/((/NetValue_Service.getNetValueOpen?page=1&num=40&sort=nav_date&asc=0&ccode=&type2=2&type3='
+# type2 = 0 基金类型为"全部"
+FUND_LIST_URL = 'http://vip.stock.finance.sina.com.cn/fund_center/data/jsonp.php/((/NetValue_Service.getNetValueOpen?sort=nav_date&asc=0&type2=0'
 
+# 获取基金历史净值 url
 FUND_HISTORY_NAV_URL = 'https://stock.finance.sina.com.cn/fundInfo/api/openapi.php/CaihuiFundInfoService.getNav?callback=(('
 
 # 字段映射关系
 keyMaps = {
+    'sname': 'name',
     'per_nav': 'perNav',
     'total_nav': 'totalNav',
     'yesterday_nav': 'yesterdayNav',
@@ -26,18 +28,36 @@ keyMaps = {
     'fund_manager': 'fundManager',
     'jjlx': 'type'
 }
-
-def getFundList():
-    r = requests.get(FUND_LIST_URL)
-    data = r.content.decode('gb2312').encode('utf-8', "ignore").decode('utf-8', 'ignore')
+#####################################################################################################
+#####################################################################################################
+# get one page fund list
+def getOnePageFund(page, pageNum):
+    r = requests.get(FUND_LIST_URL, params={'page': page, 'num': pageNum})
+    data = r.content.decode('gbk').encode('utf-8', "ignore").decode('utf-8', 'ignore')
     data = re.sub('^[\s\S]+?\\{', '{', data)
     data = re.sub('\\}\\)[\s\S]+?$', '}', data)
-    
+
     # 将 javascript 格式的 json 转为标准 json
     jsonData = demjson.decode(data, decode_object=convertKey)
-    print(jsonData)
+    #print(jsonData)
+    return {'totalNum': jsonData['total_num'], 'fundList': jsonData['data']}
 
-# 数据清洗，转换对象的 key 名称
+# get all fund list
+def getAllFundList(pageNum = 40):
+    fundList = []
+    onePageFund = getOnePageFund(1, pageNum)
+    fundList.extend(onePageFund['fundList'])
+
+    pageCount = math.ceil(onePageFund['totalNum'] / pageNum)
+    
+    if pageCount > 1:
+        for page in range(2, pageCount + 1):
+            onePageFund = getOnePageFund(page, pageNum)
+            fundList.extend(onePageFund['fundList'])
+    
+    saveToFile('fundList.json', fundList)
+
+
 def convertKey(obj):
     for k in keyMaps.keys():
         if k in obj.keys():
@@ -47,7 +67,8 @@ def convertKey(obj):
 
 #getFundList()
 
-
+#######################################################################################################
+#######################################################################################################
 # 基金净值数据格式转换
 def convertNav(obj):
     if 'fbrq' in obj:
@@ -74,7 +95,8 @@ def getOnePageFundNav(symbol, page):
 
     return {'totalNum': int(totalNum), 'navList': navList}
 
-def getFundNavBatch(symbol):
+# get a fund history nav data
+def getOneFundNavBatch(symbol):
     navList = []
     dictNav = getOnePageFundNav(symbol, 1)
     navList.extend(dictNav['navList'])
@@ -100,11 +122,71 @@ def getFundNavBatch(symbol):
     saveToFile(symbol, navList)
     #return navList
 
+#############################################################################
+#############################################################################
 
+FUND_BASIC_INFO_URL = 'https://stock.finance.sina.com.cn/fundInfo/api/openapi.php/FundPageInfoService.tabjjgk?format=json&callback=fund'
+
+# 根据基金代码获取基金基本信息
+def getFundBasicInfo(symbol):
+    r = requests.get(FUND_BASIC_INFO_URL, params = {'symbol': symbol})
+    # remove '\/', otherwise has warning: DeprecationWarning: invalid escape sequence '\/' 
+    data = re.sub(r'\\\/', '', r.text)
+    data = re.sub(r'\\"', "'", data)
+    data = data.encode('utf-8', 'ignore').decode('unicode-escape')
+    data = re.sub(r'^[\w\W]+fund\(', '', data)
+    data = re.sub(r'\)$', '', data)
+    data = re.sub(r'[\n\r]', '', data)
+    dictData = demjson.decode(data, decode_object=convertBasicInfo)
+
+    dictData['result']['data']['symbol'] = symbol
+    return dictData['result']['data']
+    #print(dictData)
+
+
+def convertBasicInfo(obj):
+    if 'symbol' in obj.keys():
+        ret = {}
+
+        ret['name'] = obj['jjjc']
+        ret['fullName'] = obj['jjqc']
+        ret['issueDate'] = obj['clrq'][:10]
+        ret['type'] = obj['Type2Name']
+        ret['companyId'] = obj['CompanyId']
+        ret['companyName'] = obj['glr']
+
+        mList = re.findall(r'mid\=(\d+)', obj['ManagerName'])
+        mmList = re.findall(r'\>(.+?)\<a\>', obj['ManagerName'])
+        ret['mamagerId'] = ','.join(mList)
+        ret['managerName'] = ','.join(mmList)
+        
+        return ret
+    return obj
+
+print(getFundBasicInfo('005636'))
+##############################################################################
+##############################################################################
+# save to file
 def saveToFile(symbol, content):
-   fobj = open(symbol + '.json', 'a', encoding="utf-8")
+   fobj = open(symbol + '.json', 'w', encoding="utf-8")
    fobj.write(str(content))
    fobj.close
 
+###############################################################################
+###############################################################################
 
-getFundNavBatch('005636')
+FUND_DOMAIN = 'localhost:8080/yeahstock/fund/'
+API_MAP = {
+    'addFund': 'addFund',
+    'saveFundNav': 'saveFundNav'
+}
+# rest method, save data to database
+def addFund(fund):
+    url = FUND_DOMAIN + API_MAP['addFund']
+    r = requests.post(url, data = fund)
+
+    print(r.text)
+
+#getOnePageFund(1, 20)
+#getAllFundList()
+#getOneFundNavBatch('005636')
